61d614832c965667cbb86cf077063f3025383264
OSE-ICE <ose1975@gmail.com>
2023-11-24 14:24:54 +0000

    bætti við brandara

--- lib/jokes.dart
+++ lib/jokes.dart
@@ -7,6 +7,7 @@ List<String> jokes = [
   "Ég keypti mér nýtt boomerang um daginn og ætlaði að losa mig við það gamla, en það tókst bara ekki.. \n --Það kom alltaf til baka...",
   "Hvað verður um ís sem er búinn að vera lengi í boxi? \n --Hann kemst á endanum í form...",
   "Gerviblómin mín dóu af því að ég gleymdi að þykjast vökva þau...",
+  "Amma hóf að ganga 10 kílómetra á dag þegar hún varð sextug.\n --Í dag höfum við ekki hugmynd um hvar hún er...",
 ];
 
 String randomJokes = jokes[Random().nextInt(jokes.length)];
