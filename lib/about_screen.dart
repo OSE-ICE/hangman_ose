992a0342f218dad42213b7fe13a4be9b30d62c55
OSE-ICE <ose1975@gmail.com>
2023-12-12 13:59:37 +0000

    kláraði about_screen.dart og bætti við link á wikipedia

--- lib/about_screen.dart
+++ lib/about_screen.dart
@@ -1,6 +1,9 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hangman_ose/widget/headline_text.dart';
+import 'package:url_launcher/link.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutScreen extends StatelessWidget {
   const AboutScreen({Key? key}) : super(key: key);
@@ -14,35 +17,73 @@ class AboutScreen extends StatelessWidget {
         title: headlineText("Um Hengimann"),
       ),
       body: Padding(
-        padding: EdgeInsets.all(12.0),
+        padding: const EdgeInsets.all(15.0),
         child: SingleChildScrollView(
-          child: Text(
-            'Hengimann er leikur þar sem þú átt að giska á orð. '
-            'Þú getur valið flokk og þá kemur orð úr þeim flokki. '
-            'Þú átt að giska á stafina í orðinu og ef þú giskar rangt þá taparðu lífi. '
-            'Þú getur bara giskað á stafina í enskum stafrófi. Þú getur ekki giskað á sama stafinn tvisvar sinnum. '
-            'Þú getur bara giskað á stafina í enskum stafrófi. Þú getur ekki giskað á sama stafinn tvisvar sinnum.'
-            'byggt á leiknum Hangman sem er búinn til af saumur og er aðgengilegur á GitHub. '
-            'Þú getur fundið kóðann á GitHub undir nafninu HangmanOSE. '
-            'Þú getur fundið kóðann á GitHub undir nafninu HangmanOSE. '
-            'þetta er bara til að fylla út plássið. '
-            'til að þurfa að setja scroolview á þetta. '
-            'veit ekki hvað ég þarf mikið af texta. '
-            'greinilega þarf ég að setja scroolview á þetta. '
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'bldskajfkasjfdlaksdjflkjasdlfkjlsdakfjkhsadfkasdjhkasdjfkasjhfksadj'
-            'þetta er kannski nóg.',
-            // set the style of the text
-            style: GoogleFonts.roboto(
-              fontSize: 20,
-              color: Colors.black,
+          child: RichText(
+            text: TextSpan(
+              children: [
+                const TextSpan(
+                  text: "Hvernig spila ég Hengimann?",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const TextSpan(
+                  text:
+                      '''\nHengimann er leikur þar sem þú átt að giska á orð.  Þú átt að giska á stafina í orðinu og ef þú giskar rangt þá taparðu lífi.   Þú getur ekki giskað á sama stafinn tvisvar sinnum. Þú hefur 7 tilraunir til að giska á orðið.
+                      ''',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black87,
+                  ),
+                ),
+                const TextSpan(
+                  text: "\nLesa nánar um leikinn",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                TextSpan(
+                  text: "\nSmelltu hér til að lesa nánar um leikinn(á ensku)\n",
+                  style: const TextStyle(
+                    fontSize: 20,
+                    color: Colors.blue,
+                    decoration: TextDecoration.underline,
+                  ),
+                  recognizer: TapGestureRecognizer()
+                    ..onTap = () async {
+                      const url =
+                          'https://en.wikipedia.org/wiki/Hangman_(game)';
+                      final uri = Uri.parse(url);
+                      if (await canLaunchUrl(uri)) {
+                        await launchUrl(uri);
+                      } else {
+                        throw 'Could not launch $url';
+                      }
+                    },
+                ),
+                const TextSpan(
+                  text: "\nSaga Hengimanns",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black87,
+                  ),
+                ),
+                const TextSpan(
+                  text:
+                      '''\nÞað er ekki alveg ljóst hvar leikurinn kom fyrst fram, en hér eru upplýsingar um uppruna hans:\nHugtakið Hangman var mótað af Thomas Schelling í bók hans frá árinu 1960, The Strategy of Conflict. Þetta hugtak hefur öðlast mikinn stuðning innan stjórnmálafræða. Stefnumarkandi leikir breyta stefnu leiksins þannig að hann er í hag leikmannsins.\nLeikurinn í námi barna: Hangman hefur einnig verið mikilvægur leikur í lífi yngri barna. Hann hjálpar þeim við vitsmunaþroska, hreyfiþroska og félagsþroska. Þessir leikir eru frjálsir, könnunarleikir, og þau eru aðal uppspretta þroska barnanna.\nÞannig að Hangman hefur ekki bara skemmt börnum og fullorðnum, heldur hefur hann einnig verið gagnlegur í námi og þroska. 
+                      ''',
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black87,
+                  ),
+                ),
+              ],
             ),
           ),
         ),
