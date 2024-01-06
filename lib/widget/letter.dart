eb54fee0494962709394b69e392c705029af36f8
OSE-ICE <ose1975@gmail.com>
2024-01-06 13:15:13 +0000

    miðja stafi í orði

--- lib/widget/letter.dart
+++ lib/widget/letter.dart
@@ -4,7 +4,7 @@ Widget letter(String character, bool hidden, bool isSpace) {
   return Container(
     height: 40,
     width: 40,
-    padding: const EdgeInsets.fromLTRB(17, 10, 5, 5),
+    padding: const EdgeInsets.fromLTRB(14, 7, 5, 5),
     decoration: BoxDecoration(
       color: isSpace ? Colors.transparent : Colors.indigo.shade400,
       borderRadius: BorderRadius.circular(4.0),
